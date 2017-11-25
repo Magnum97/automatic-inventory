e1238d5a2cd019fb4a91a080f28ba4e36aa6c9af
Con Reptio <conreptio@gmail.com>
2017-11-25 09:55:44 +0100

    Add main class

---
+++ src/main/java/com/neolumia/autoinventory/AutoPlugin.java
@@ -0,0 +1,11 @@
+package com.neolumia.autoinventory;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AutoPlugin extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+    getLogger().info("Hello World!");
+  }
+}
